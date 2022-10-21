e221ec6969b98035bc650e9361a084a20bba6071
Hz <1165929365@qq.com>
2022-10-22 00:35:07 +0800

    Ininitized

---
+++ src/main/java/com/hz/SellerCloud/SellerCloudApplication.java
@@ -0,0 +1,13 @@
+package com.hz.SellerCloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellerCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellerCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hz/SellerCloud/SellerCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hz.SellerCloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellerCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
