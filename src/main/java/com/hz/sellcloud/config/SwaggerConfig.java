2e037acec56740e1429793157996a9a6a5213107
Hz <1165929365@qq.com>
2022-10-28 16:11:15 +0800

    Swagger配置

---
+++ src/main/java/com/hz/sellcloud/config/SwaggerConfig.java
@@ -0,0 +1,50 @@
+package com.hz.sellcloud.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    /**
+     * 创建API应用
+     * apiInfo() 增加API相关信息
+     * 通过select()函数返回一个ApiSelectorBuilder实例,用来控制哪些接口暴露给Swagger来展现，
+     * 本例采用指定扫描的包路径来定义指定要建立API的目录。
+     *
+     * @return
+     */
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.hz.sellcloud.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    /**
+     * 创建该API的基本信息（这些基本信息会展现在文档页面中）
+     * 访问地址：http://项目实际地址/swagger-ui.html
+     * @return
+     */
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("销售云平台及接口文档")
+                .description("（设计思路）销售云平台提供API接口访问添加数据，将数据缓存到Redis缓存，在Redis缓存过大或者定时任务时间到达时" +
+                        "进行清理，持久化到数据库当中，鉴于前端传入的数据特性，商品种类繁多但信息结构先对稳定，可以通过MapRedce任务运算后再计入数据库表当中")
+                .contact(new Contact("Hz","http://localhost:8080/sellcloud","1165929365@qq.com"))
+                .version("1.0")
+                .build();
+    }
+}
